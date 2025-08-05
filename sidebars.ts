18f0721621b3e6b3274f18460bf863d52cebcaa2
bhjj <3317038611@qq.com>
2025-08-06 00:52:48 +0800

    ✨feat: - 添加简历优化指南到文档 - 更新侧边栏配置，增加新的文档链接

--- sidebars.ts
+++ sidebars.ts
@@ -1,10 +1,26 @@
 import type {SidebarsConfig} from '@docusaurus/plugin-content-docs';
 
+
 const sidebars: SidebarsConfig = {
   tutorialSidebar: [
-    'intro',
-    'create-resume',
-    'hunter-feature',
+    "intro",
+    "create-resume",
+    {
+      type: "category",
+      label: "如何优化简历",
+      link: {
+        type: "generated-index",
+        slug: "/optimize-resume",
+        description: "💡 我们提供了三种工具来优化简历",
+      },
+      collapsed: true,
+      items: [
+        "optimize-resume/smart-physical-examination",
+        "optimize-resume/content-doctor",
+        "optimize-resume/project-workshop",
+      ],
+    },
+    "hunter-feature",
   ],
 };
 
