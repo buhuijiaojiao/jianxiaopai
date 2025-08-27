a360d6c029e5d4112a4284f14d2cd134c804cfaa
bhjj <3317038611@qq.com>
2025-08-27 20:26:34 +0800

    Revert "feat: 添加重置密码功能指南并更新所有链接 - 创建重置密码功能使用指南文档 - 将所有http://8.153.173.210:5173链接替换为https://jianlipai.com/ - 更新侧边栏配置，添加新的文档链接"
    This reverts commit 85f2cb2533c307749ed951494694bf735cbec900.

--- sidebars.ts
+++ sidebars.ts
@@ -3,10 +3,29 @@ import type {SidebarsConfig} from '@docusaurus/plugin-content-docs';
 
 const sidebars: SidebarsConfig = {
   tutorialSidebar: [
-    'intro',
-    'create-resume',
-    'hunter-feature',
-    'reset-password',
+    "upgrade-announcement",
+    "create-resume",
+    "jianxiaojian-guide",
+    "zhixiaopai-preview",
+    "zhixiaopai-guide",
+    "invitation-code-guide",
+    "delivery-invitation-code-guide",
+    {
+      type: "category",
+      label: "如何优化简历",
+      link: {
+        type: "generated-index",
+        slug: "/optimize-resume",
+        description: "💡 我们提供了三种工具来优化简历",
+      },
+      collapsed: true,
+      items: [
+        "optimize-resume/smart-physical-examination",
+        "optimize-resume/content-doctor",
+        "optimize-resume/project-workshop",
+      ],
+    },
+    "hunter-feature",
   ],
 };
 
